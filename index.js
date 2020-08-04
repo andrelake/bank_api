97e1e4b2c00d81fd4a76150b3211b196ce320bfa
andrelake <andre.lake@hotmail.com>
2020-08-04 20:48:28 -0300

    Json db config

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
+import { promises as fs } from 'fs';
 import express from 'express';
+
+import accRouter from './routes/account.js';
+
+const { readFile, writeFile } = fs;
 
 const app = express();
 app.use(express.json());
 
-app.listen(3030, () => {});
+app.use('./account', accRouter);
+
+app.listen(3030, async () => {
+  try {
+    await readFile('accounts.json');
+    console.log('API started. File readed.');
+  } catch (err) {
+    const initialJson = {
+      nextId: 1,
+      accounts: [],
+    };
+
+    writeFile('accounts.json', JSON.stringify(initialJson))
+      .then(() => {
+        console.log('API started. File created.');
+      })
+      .catch((err) => console.log(err));
+  }
+});
